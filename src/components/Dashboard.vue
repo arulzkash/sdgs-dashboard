fc146e2dc640c8ba23d739eb777e53900c646c72
boyaditya <boy.adityarr@gmail.com>
2025-01-10 16:49:18 +0700

    dashboard light mode

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="dashboard" class="h-full">
     <!-- PASTE IFRAME SNIPPET HERE-->
-     <iframe style="background: #21313C;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);width: 100vw;height: 100vh;"  src="https://charts.mongodb.com/charts-project-0-dxalyto/embed/dashboards?id=e07846ad-7c2c-42bf-97a4-3c13fa3ba325&theme=dark&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=true&scalingWidth=fixed&scalingHeight=fixed"></iframe>
+  <iframe style="background: #F1F5F4;border: none;border-radius: 2px;box-shadow: 0 2px 10px 0 rgba(70, 76, 79, .2);width: 100vw;height: 100vh;"  src="https://charts.mongodb.com/charts-project-0-dxalyto/embed/dashboards?id=e07846ad-7c2c-42bf-97a4-3c13fa3ba325&theme=light&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=true&scalingWidth=fixed&scalingHeight=fixed"></iframe>
      
   </div>
 </template>
